d2b7c0fa6dcad4cc020a375219b567bb8dc1c27a
MUNKHJARGAL Manlaibayar <24b1num0285@stud.num.edu.mn>
2026-05-06 17:34:36 +0800

    .

--- main.cpp
+++ main.cpp
@@ -51,5 +51,26 @@ int main() {
     cout << endl;
     emp3.printInfo();
 
+    // Task 1: for the objects of Employee class configure spouse and child.
+    Spouse sp2("Saraa", "999-888", 28, "2021-02-14");
+    emp2.setSpouse(&sp2);
+    Child c3("Bold", "777-666", 3, "Ball");
+    emp2.addChild(&c3);
+
+    Spouse sp3("Tulga", "555-444", 33, "2019-10-10");
+    Child c4("Maral", "333-222", 5, "Book");
+    Child c5("Anar", "111-000", 1, "Toy");
+    emp3.setSpouse(&sp3);
+    emp3.addChild(&c4);
+    emp3.addChild(&c5);
+
+    // Task 2: for each object of Employee class print all of it's information 
+    // (0..1, 0..n, and 1, 1..n relationships are to be programmed correctly)
+    emp1.printInfo();
+    cout << endl;
+    emp2.printInfo();
+    cout << endl;
+    emp3.printInfo();
+
     return 0;
 }
